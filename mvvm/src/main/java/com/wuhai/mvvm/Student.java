8c5396228ec525fa547f7b12bea73ad103e95b57
wuhai <wuhai@qiandaodao.com>
2019-11-09 17:51:17 +0800

    mvvm

---
+++ mvvm/src/main/java/com/wuhai/mvvm/Student.java
@@ -0,0 +1,71 @@
+package com.wuhai.mvvm;
+
+import android.widget.ImageView;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.BindingAdapter;
+
+import com.squareup.picasso.Picasso;
+
+
+/**
+ * Created by wuhai on 2017/08/14 15:47.
+ * 描述：
+ */
+
+public class Student extends BaseObservable {
+
+    private String name;
+    private String addr;
+    private String photo;
+
+    public Student() {
+    }
+
+    public Student(String name, String addr) {
+        this.name = name;
+        this.addr = addr;
+    }
+
+    public Student(String name, String addr, String photo) {
+        this(name,addr);
+        this.photo = photo;
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    @Bindable
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+        notifyPropertyChanged(BR.addr);
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @BindingAdapter("photo")
+    public static void getInternetImage(ImageView iv, String url) {
+        Picasso.with(iv.getContext()).
+                load(url).
+                error(R.mipmap.weixin).
+                into(iv);
+    }
+}
