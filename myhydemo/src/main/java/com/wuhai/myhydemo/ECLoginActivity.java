8cda597990e500c8aa1eaf31a16d6a72959feef1
wuhai <wuhai@qiandaodao.com>
2019-11-24 22:24:04 +0800

    环信 简单入门

---
+++ myhydemo/src/main/java/com/wuhai/myhydemo/ECLoginActivity.java
@@ -0,0 +1,182 @@
+package com.wuhai.myhydemo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+
+public class ECLoginActivity extends AppCompatActivity {
+    // 弹出框
+    private ProgressDialog mDialog;
+    // username 输入框
+    private EditText mUsernameEdit;
+    // 密码输入框
+    private EditText mPasswordEdit;
+    // 注册按钮
+    private Button mSignUpBtn;
+    // 登录按钮
+    private Button mSignInBtn;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 是否曾经登录过
+        if (EMClient.getInstance().isLoggedInBefore()) {
+            Log.e(ECApplication.TAG,"自动登录，曾经登录过");
+            Intent intent = new Intent(this, ECMainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.activity_login);
+
+        mUsernameEdit = findViewById(R.id.ec_edit_username);
+        mPasswordEdit = findViewById(R.id.ec_edit_password);
+
+        mSignUpBtn = findViewById(R.id.ec_btn_sign_up);
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+
+        mSignInBtn = findViewById(R.id.ec_btn_sign_in);
+        mSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+    }
+
+    /**
+     * 登录
+     */
+    private void signIn() {
+        EMClient.getInstance().login(mUsernameEdit.getText().toString().trim(),
+                mPasswordEdit.getText().toString().trim(),new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.d(ECApplication.TAG, "登录聊天服务器成功！");
+
+                Intent intent = new Intent(ECLoginActivity.this, ECMainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.d(ECApplication.TAG, "onError code="+code+",message="+message);
+            }
+        });
+    }
+
+    /**
+     * 注册
+     */
+    private void signUp() {
+        // 注册是耗时过程，所以要显示一个dialog来提示下用户
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("注册中，请稍后...");
+        mDialog.show();
+
+        new Thread(new Runnable() {
+            @Override public void run() {
+                try {
+                    String username = mUsernameEdit.getText().toString().trim();
+                    String password = mPasswordEdit.getText().toString().trim();
+                    EMClient.getInstance().createAccount(username, password);
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            if (!ECLoginActivity.this.isFinishing()) {
+                                mDialog.dismiss();
+                            }
+                            Toast.makeText(ECLoginActivity.this, "注册成功", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            if (!ECLoginActivity.this.isFinishing()) {
+                                mDialog.dismiss();
+                            }
+                            /**
+                             * 关于错误码可以参考官方api详细说明
+                             * http://www.easemob.com/apidoc/android/chat3.0/classcom_1_1hyphenate_1_1_e_m_error.html
+                             */
+                            int errorCode = e.getErrorCode();
+                            String message = e.getMessage();
+                            Log.d("lzan13",
+                                    String.format("sign up - errorCode:%d, errorMsg:%s", errorCode,
+                                            e.getMessage()));
+                            switch (errorCode) {
+                                // 网络错误
+                                case EMError.NETWORK_ERROR:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "网络错误 code: " + errorCode + ", message:" + message,
+                                            Toast.LENGTH_LONG).show();
+                                    break;
+                                // 用户已存在
+                                case EMError.USER_ALREADY_EXIST:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "用户已存在 code: " + errorCode + ", message:" + message,
+                                            Toast.LENGTH_LONG).show();
+                                    break;
+                                // 参数不合法，一般情况是username 使用了uuid导致，不能使用uuid注册
+                                case EMError.USER_ILLEGAL_ARGUMENT:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "参数不合法，一般情况是username 使用了uuid导致，不能使用uuid注册 code: "
+                                                    + errorCode
+                                                    + ", message:"
+                                                    + message, Toast.LENGTH_LONG).show();
+                                    break;
+                                // 服务器未知错误
+                                case EMError.SERVER_UNKNOWN_ERROR:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "服务器未知错误 code: " + errorCode + ", message:" + message,
+                                            Toast.LENGTH_LONG).show();
+                                    break;
+                                case EMError.USER_REG_FAILED:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "账户注册失败 code: " + errorCode + ", message:" + message,
+                                            Toast.LENGTH_LONG).show();
+                                    break;
+                                default:
+                                    Toast.makeText(ECLoginActivity.this,
+                                            "ml_sign_up_failed code: " + errorCode + ", message:" + message,
+                                            Toast.LENGTH_LONG).show();
+                                    break;
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+}
