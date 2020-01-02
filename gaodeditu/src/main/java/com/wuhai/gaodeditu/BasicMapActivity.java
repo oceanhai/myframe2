1e0b51f2637bdd9aa7588f5b25bbaff025249485
wuhai <wuhai@eq102.com>
2020-01-02 09:52:05 +0800

    高德地图

--- gaodeditu/src/main/java/com/wuhai/gaodeditu/BasicMapActivity.java
+++ gaodeditu/src/main/java/com/wuhai/gaodeditu/BasicMapActivity.java
@@ -2,16 +2,42 @@ package com.wuhai.gaodeditu;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.animation.Animation;
+import com.amap.api.maps.model.animation.TranslateAnimation;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.help.Inputtips;
+import com.amap.api.services.help.InputtipsQuery;
+import com.amap.api.services.help.Tip;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 作者 wuhai
@@ -20,12 +46,20 @@ import com.amap.api.maps.model.MyLocationStyle;
  *
  * 描述：显示地图，定位
  */
-public class BasicMapActivity extends AppCompatActivity {
+public class BasicMapActivity extends AppCompatActivity implements TextWatcher, Inputtips.InputtipsListener {
 
     private static final String TAG = "BasicMapActivity";
 
     private MapView mMapView;//容器
     private AMap aMap;//控制类
+
+    //输入提示查询   功能
+    private String city = "北京";
+    private AutoCompleteTextView mKeywordText;
+    private ListView minputlist;
+
+    //Marker 动画功能
+    Marker screenMarker = null;
 
     /**
      * @param context
@@ -54,10 +88,47 @@ public class BasicMapActivity extends AppCompatActivity {
 
     private void init(Bundle savedInstanceState) {
         mMapView = (MapView) findViewById(R.id.map);
+
+        //输入提示
+        minputlist = (ListView)findViewById(R.id.inputlist);
+        mKeywordText = (AutoCompleteTextView)findViewById(R.id.input_edittext);
+        mKeywordText.addTextChangedListener(this);
+
         mMapView.onCreate(savedInstanceState);// 此方法必须重写
         aMap = mMapView.getMap();
+        aMap.setOnMapLoadedListener(new AMap.OnMapLoadedListener() {
+            @Override
+            public void onMapLoaded() {
+                addMarkersToMap();
+            }
+        });
+
+        // 设置可视范围变化时的回调的接口方法
+        aMap.setOnCameraChangeListener(new AMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition position) {
+
+            }
+
+            @Override
+            public void onCameraChangeFinish(CameraPosition postion) {
+                //屏幕中心的Marker跳动
+                startJumpAnimation();
+            }
+        });
 
         location();
+
+    }
+
+    private void addMarkersToMap() {
+        LatLng latLng = aMap.getCameraPosition().target;
+        Point screenPosition = aMap.getProjection().toScreenLocation(latLng);
+        screenMarker = aMap.addMarker(new MarkerOptions()
+                .anchor(0.5f,0.5f)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.purple_pin)));
+        //设置Marker在屏幕上,不跟随地图移动
+        screenMarker.setPositionByPixels(screenPosition.x,screenPosition.y);
     }
 
     /**
@@ -72,7 +143,7 @@ public class BasicMapActivity extends AppCompatActivity {
         myLocationStyle.interval(2000);
 
 //        //只定位一次。
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);
 //        //定位一次，且将视角移动到地图中心点。
 //        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;
 //        //连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
@@ -96,7 +167,10 @@ public class BasicMapActivity extends AppCompatActivity {
         aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
-                Log.e(TAG,"location:"+location.toString());
+                Log.e(TAG,"定位信息 location:"+location.toString());
+                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                aMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(
+                        latLng, 13, 0, 0)));
             }
         });
     }
@@ -135,5 +209,119 @@ public class BasicMapActivity extends AppCompatActivity {
 
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        String newText = s.toString().trim();
+        if(TextUtils.isEmpty(newText)){
+            minputlist.setVisibility(View.GONE);
+        }else{
+            minputlist.setVisibility(View.VISIBLE);
+
+            InputtipsQuery inputquery = new InputtipsQuery(newText, city);
+            inputquery.setCityLimit(true);
+            Inputtips inputTips = new Inputtips(BasicMapActivity.this, inputquery);
+            inputTips.setInputtipsListener(this);
+            inputTips.requestInputtipsAsyn();
+        }
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    /**
+     * 输入提示结果的回调
+     * @param tipList
+     * @param rCode
+     */
+    @Override
+    public void onGetInputtips(List<Tip> tipList, int rCode) {
+        if (rCode == AMapException.CODE_AMAP_SUCCESS) {
+            final List<HashMap<String, String>> listString = new ArrayList<HashMap<String, String>>();
+            if(tipList != null) {
+                int size = tipList.size();
+                for (int i = 0; i < size; i++) {
+                    Tip tip = tipList.get(i);
+                    if(tip != null) {
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put("name", tip.getName());
+                        map.put("address", tip.getDistrict());
+                        Log.e(TAG,tip.getName()+","+tip.getDistrict()+","+
+                                tip.getPoint().getLatitude()+","+tip.getPoint().getLongitude());
+                        listString.add(map);
+                    }
+                }
+                SimpleAdapter aAdapter = new SimpleAdapter(getApplicationContext(), listString, R.layout.item_layout,
+                        new String[]{"name", "address"}, new int[]{R.id.poi_field_id, R.id.poi_value_id});
+
+                minputlist.setAdapter(aAdapter);
+                minputlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        HashMap<String, String> map = listString.get(position);
+                        Log.e(TAG, "你点击了列表:"+map.get("name")+","+map.get("address"));
+                    }
+                });
+                aAdapter.notifyDataSetChanged();
+            }
+
+        } else {
+            Log.e(TAG, "rCode="+rCode);
+        }
+    }
+
+
+    /**
+     * 屏幕中心marker 跳动
+     */
+    public void startJumpAnimation() {
+
+        if (screenMarker != null ) {
+            //根据屏幕距离计算需要移动的目标点
+            final LatLng latLng = screenMarker.getPosition();
+            Point point =  aMap.getProjection().toScreenLocation(latLng);
+            point.y -= dip2px(this,125);
+            LatLng target = aMap.getProjection()
+                    .fromScreenLocation(point);
+            //使用TranslateAnimation,填写一个需要移动的目标点
+            Animation animation = new TranslateAnimation(target);
+            animation.setInterpolator(new Interpolator() {
+                @Override
+                public float getInterpolation(float input) {
+                    // 模拟重加速度的interpolator
+                    if(input <= 0.5) {
+                        return (float) (0.5f - 2 * (0.5 - input) * (0.5 - input));
+                    } else {
+                        return (float) (0.5f - Math.sqrt((input - 0.5f)*(1.5f - input)));
+                    }
+                }
+            });
+            //整个移动所需要的时间
+            animation.setDuration(600);
+            //设置动画
+            screenMarker.setAnimation(animation);
+            //开始动画
+            screenMarker.startAnimation();
+
+            Log.e(TAG,"图片原latLng:"+latLng.latitude+","+latLng.longitude+
+                    ";移动到target："+target.latitude+","+target.longitude);
+        } else {
+            Log.e(TAG,"screenMarker is null");
+        }
+    }
+
+    //dip和px转换
+    private static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 }
