42f3131ff35220e6615ea7afe3742a8d982fa930
wuhai <wuhai@eq102.com>
2020-02-18 20:59:40 +0800

    弹窗

---
+++ app/src/main/java/com/wuhai/myframe2/ui/popwindow/FloatingWindow.java
@@ -0,0 +1,105 @@
+package com.wuhai.myframe2.ui.popwindow;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+import com.wuhai.myframe2.R;
+
+
+/**
+* 非随点击位置 PopupWindow
+* @author wuhai
+* create at 2016/2/26 16:40
+*/
+public class FloatingWindow {
+
+	private Context mContext;
+	private PopupWindow mWindow;
+	private View mParent;
+	private View mContentView;
+	
+	private IFloatingOperation mOperCallback;
+
+	private int[] location;
+
+	public FloatingWindow(Context context, View parent){
+		mContext = context;
+		mParent = parent;
+
+		location = new int[2];
+		parent.getLocationOnScreen(location);
+		
+		mContentView = View.inflate(mContext, R.layout.popwindow_floating_window, null);
+		mWindow = new PopupWindow(mContentView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		
+		mContentView.findViewById(R.id.read_fw_delete).setOnClickListener(mClickListener);
+	}
+
+	/**
+	 *
+	 * @param x
+	 * @param y
+	 * @param direction	方向 true：箭头向上；false:箭头向上
+	 */
+	public void show(int x, int y, boolean direction){
+		setBackground(direction);
+		mWindow.showAtLocation(mParent, Gravity.NO_GRAVITY, location[0], location[1]-mWindow.getHeight());
+	}
+	
+	private void setBackground(boolean direction) {
+		View view = mContentView.findViewById(R.id.read_fw_bglayout);
+		if (direction) {
+//			view.setBackgroundResource(R.drawable.reader_note_arrow_up_empty);
+		} else {
+			view.setBackgroundResource(R.drawable.note_arrow_down_empty);
+		}
+	}
+	
+	public void hide(){
+		if(mWindow != null){
+			mWindow.dismiss();
+		}
+	}
+	
+	public boolean isShowing(){
+		return mWindow != null && mWindow.isShowing();
+	}
+	
+	/**
+	 * 回调接口
+	 * @param l
+	 */
+	public void setFloatingOperation(IFloatingOperation l){
+		mOperCallback = l;
+	}
+	
+	final OnClickListener mClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if(mOperCallback == null){
+				return;
+			}
+			switch (v.getId()) {
+				case R.id.read_fw_delete:
+					mOperCallback.onDelete();
+					break;
+				default:
+					break;
+			}
+			hide();
+		}
+	};
+
+	public interface IFloatingOperation {
+
+		/**
+		 * 删除
+		 */
+		void onDelete();
+
+	}
+}
--- app/src/main/java/com/wuhai/myframe2/ui/popwindow/PopWindowActivity.java
+++ app/src/main/java/com/wuhai/myframe2/ui/popwindow/PopWindowActivity.java
@@ -3,8 +3,10 @@ package com.wuhai.myframe2.ui.popwindow;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.wuhai.myframe2.R;
 import com.wuhai.myframe2.ui.base.BaseActivity;
@@ -18,9 +20,9 @@ import butterknife.ButterKnife;
  * 创建日期 2019/4/3 11:45
  * <p>
  * 描述：PopWindow
- *
- *  Android Popwindow使用总结  没细看
- *  https://www.jianshu.com/p/3812ff5ef272
+ * <p>
+ * Android Popwindow使用总结  没细看
+ * https://www.jianshu.com/p/3812ff5ef272
  */
 public class PopWindowActivity extends BaseActivity implements View.OnClickListener {
 
@@ -28,6 +30,10 @@ public class PopWindowActivity extends BaseActivity implements View.OnClickListe
     Button btn01;
     @BindView(R.id.btn02)
     Button btn02;
+    @BindView(R.id.tv01)
+    TextView tv01;
+    @BindView(R.id.tv02)
+    TextView tv02;
 
     private MorePopWindow morePopWindow;
     private ListPopWindow mListPopWindow;
@@ -70,6 +76,9 @@ public class PopWindowActivity extends BaseActivity implements View.OnClickListe
     private void setListener() {
         btn01.setOnClickListener(this);
         btn02.setOnClickListener(this);
+
+        tv01.setOnClickListener(this);
+        tv02.setOnClickListener(this);
     }
 
     @Override
@@ -92,6 +101,30 @@ public class PopWindowActivity extends BaseActivity implements View.OnClickListe
                     mListPopWindow = new ListPopWindow(this);
                 }
                 mListPopWindow.showPopupWindow(v);
+                break;
+            case R.id.tv01:
+                FloatingWindow floatingWindow = new FloatingWindow(this, v);
+                floatingWindow.show(0, 0, false);
+                floatingWindow.setFloatingOperation(new FloatingWindow.IFloatingOperation() {
+                    @Override
+                    public void onDelete() {
+
+                    }
+                });
+                break;
+            case R.id.tv02:
+                PositionCanChangePopupWindow positionCanChangePopupWindow =
+                        new PositionCanChangePopupWindow(this, v, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                            }
+                        });
+                Log.e(TAG, "v.getX()="+v.getX()+", v.getY()="+v.getY());
+                Log.e(TAG, "v.getLeft()="+v.getLeft()+", v.getTop()="+v.getTop()+
+                        ",v.getRight()="+v.getRight()+",v.getBottom()="+v.getBottom());
+                positionCanChangePopupWindow.
+                        showPopupWindow((v.getRight()+v.getLeft())/2,(v.getBottom()+v.getTop())/2);
                 break;
         }
     }
