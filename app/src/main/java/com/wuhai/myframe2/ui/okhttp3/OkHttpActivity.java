99a6b582713506d39c08ffd8e209fc55eb7dfd0c
wuhai <wuhaimengxiang@163.com>
2022-10-25 09:43:05 +0800

    other okthttp

--- app/src/main/java/com/wuhai/myframe2/ui/okhttp3/OkHttpActivity.java
+++ app/src/main/java/com/wuhai/myframe2/ui/okhttp3/OkHttpActivity.java
@@ -39,6 +39,10 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okio.BufferedSink;
 
+/**
+ * Android OkHttp3 知识总结
+ *
+ */
 public class OkHttpActivity extends BaseActivity implements View.OnClickListener {
 
     private final static String TAG = "OkHttpActivity";
@@ -202,20 +206,26 @@ public class OkHttpActivity extends BaseActivity implements View.OnClickListener
     }
 
     /**
-     * "application/x-www-form-urlencoded"，是默认的MIME内容编码类型，一般可以用于所有的情况，但是在传输比较大的二进制或者文本数据时效率低。
+     * "application/x-www-form-urlencoded"，是默认的MIME内容编码类型，一般可以用于所有的情况，
+     * 但是在传输比较大的二进制或者文本数据时效率低。
      * 这时候应该使用"multipart/form-data"。如上传文件或者二进制数据和非ASCII数据。
      */
-    public static final MediaType MEDIA_TYPE_NORAML_FORM = MediaType.parse("application/x-www-form-urlencoded;charset=utf-8");
+    public static final MediaType MEDIA_TYPE_NORAML_FORM =
+            MediaType.parse("application/x-www-form-urlencoded;charset=utf-8");
 
     //既可以提交普通键值对，也可以提交(多个)文件键值对。
-    public static final MediaType MEDIA_TYPE_MULTIPART_FORM = MediaType.parse("multipart/form-data;charset=utf-8");
+    public static final MediaType MEDIA_TYPE_MULTIPART_FORM =
+            MediaType.parse("multipart/form-data;charset=utf-8");
 
     //只能提交二进制，而且只能提交一个二进制，如果提交文件的话，只能提交一个文件,后台接收参数只能有一个，而且只能是流（或者字节数组）
-    public static final MediaType MEDIA_TYPE_STREAM = MediaType.parse("application/octet-stream");
+    public static final MediaType MEDIA_TYPE_STREAM =
+            MediaType.parse("application/octet-stream");
 
-    public static final MediaType MEDIA_TYPE_TEXT = MediaType.parse("text/plain;charset=utf-8");
+    public static final MediaType MEDIA_TYPE_TEXT =
+            MediaType.parse("text/plain;charset=utf-8");
 
-    public static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json;charset=utf-8");
+    public static final MediaType MEDIA_TYPE_JSON =
+            MediaType.parse("application/json;charset=utf-8");
 
     /**
      * POST String
@@ -232,7 +242,7 @@ public class OkHttpActivity extends BaseActivity implements View.OnClickListener
         String str = "AndroidContent";
         RequestBody requestBody = RequestBody.create(MEDIA_TYPE_TEXT, str);
         Request requestPost = new Request.Builder()
-                .url("https://github.com/oceanhai")
+                .url("https://www.wanandroid.com/banner/json")
                 .post(requestBody)
                 .build();
 
